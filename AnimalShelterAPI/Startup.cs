c6cb0bc76927205b1d54a227d4b5cd8d559c331f
Ryan Walker <ryandanielwalker@gmail.com>
2021-06-11 08:52:33 -0700

    Reconfigure Startup to include MySql and Swagger

--- AnimalShelterAPI/Startup.cs
+++ AnimalShelterAPI/Startup.cs
@@ -11,6 +11,8 @@ using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
 using Microsoft.OpenApi.Models;
+using AnimalShelterAPI.Models;
+using Microsoft.EntityFrameworkCore;
 
 namespace AnimalShelterAPI
 {
@@ -28,11 +30,15 @@ namespace AnimalShelterAPI
     {
 
       services.AddControllers();
+      services.AddDbContext<AnimalShelterAPIContext>(opt =>
+                opt.UseMySql(Configuration["ConnectionStrings:DefaultConnection"], ServerVersion.AutoDetect(Configuration["ConnectionStrings:DefaultConnection"])));
+      services.AddControllers();
       services.AddSwaggerGen(c =>
-      {
-        c.SwaggerDoc("v1", new OpenApiInfo { Title = "AnimalShelterAPI", Version = "v1" });
-      });
+        {
+          c.SwaggerDoc("v1", new OpenApiInfo { Title = "AnimalShelterAPI", Version = "v1" });
+        });
     }
+
 
     // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
     public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
